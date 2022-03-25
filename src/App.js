9541333ae4683cb51baab0fd544bcd7585b850c0
weicheng lee <z82111712@gmail.com>
2022-03-24 20:49:10 -0700

    initial todos for useReducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useReducer, useState } from 'react'
+
+const ACTION = {
+  ADD_TODO: 'add-todo'
+}
+
+const reducer = () => {
+
+}
 
 function App() {
+  const [name, setName] = useState("")
+  const [state, dispatch] = useReducer(reducer, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
